8d481b69161c3fc9c38b36c6327c22f245b1241f
Sam Spain <sam@samspain.com>
2022-09-03 17:39:00 +0100

    Colour for main page

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="flex h-screen bg-blue-100">
+    <div class="flex h-screen bg-primary">
         <div class="m-auto">
             <form>
                 <div class="text-7xl">
-                    <label for="invitationCodeInput">Invitation Code:</label>
-                    <input id="invitationCodeInput" placeholder="Your code here" v-model="invitationCode" />
+                    <input id="invitationCodeInput" placeholder="Your code here" v-model="invitationCode" class="bg-primary-dark placeholder-primary text-primary-darker" />
                 </div>
+                <br />
                 <div>
                     <button type="button" v-on:click="goToNavigation"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-800">Go
-                        To Invite</button>
+                        class="text-secondary hover:text-secondary-dark bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-secondary-dark dark:hover:bg-secondary focus:outline-none dark:focus:ring-blue-800" >
+                        Go To Invite
+                    </button>
                 </div>
             </form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,30 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'primary': {
+        DEFAULT: "#A1BDD7",
+        dark: "#7DA4C7",
+        darker: "#598BB8"
+      },
+      'secondary': {
+        DEFAULT: "#D7BBA1",
+        dark: "#C8A17D"
+      },
+      "information": {
+        DEFAULT: "#A1A2D7",
+        dark: "#7D7FC8"
+      },
+      "success": {
+        DEFAULT: "#A1D7D6",
+        dark: "#7DC8C6"
+      },
+      "error": {
+        DEFAULT: "#D7A1BD",
+        dark: "#C87DA4"
+      }
+
+    }
   },
   plugins: [],
 }
