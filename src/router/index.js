9f6953b7835dda6a54160f5adf87619d3cc2cc5e
Sam Spain <sam@samspain.com>
2022-09-03 10:23:22 +0100

    Missed files from last commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import IndexView from "../views/IndexView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "Index",
+      component: IndexView
+    },
     {
       path: '/invitation/:userAccessToken',
       name: 'home',
