c23a01cfa3bfcdabda8beaa505c5ae065dc3bb46
Sam Spain <sam@samspain.com>
2022-09-11 01:53:37 +0100

    Convert to https

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://gospainwedding.com/",
+  base: "https://gospainwedding.com/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
