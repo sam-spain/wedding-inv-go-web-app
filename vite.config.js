ca8901ccb1147527c62e07bcc153eaac0e80885f
Sam Spain <sam@samspain.com>
2022-09-11 01:28:20 +0100

    Custom url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "http://gospainwedding.com/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
