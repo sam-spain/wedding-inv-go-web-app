2835a20284e28cbae63dd6b2ace9c93abc4c3dd6
Sam Spain <sam@samspain.com>
2022-10-02 01:03:37 +0100

    Add base url to assign

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import './index.css'
 Axios.defaults.withCredentials = true;
+let baseURL;
 if (process.env.NODE_ENV === "production") {
     baseURL = "https://api.gospainwedding.com";
 } else {
