33148b0c5e297c5b3d86723d744459821dc18efa
Sam Spain <sam@samspain.com>
2022-10-17 21:59:41 +0100

    Add life pieces before planned wedding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,100 @@
 <template>
   <div>
-    <h1 class=" bg-primary-darkest text-8xl leading-none text-white md:text-5xl lg:text-6xl">Maayong Adlaw, {{ model.enteredName
+    <h1 class=" bg-primary-darkest text-8xl leading-none text-white md:text-5xl lg:text-6xl">Maayong Adlaw, {{
+    model.enteredName
     }}</h1>
     <h1 class="bg-primary-darkest text-6xl leading-none text-white md:text-4xl lg:text-6xl">We would like you to join in
       our story...</h1>
     <div class="bg-primary-darkest h-12"></div>
-    <div class="text-4xl col-start-2 col-span-4">Karen and Sam's first date - March 2018</div>
+    <div class="bg-white text-4xl col-start-2 col-span-4">Karen and Sam's first date - March 2018</div>
     <div class="bg-white grid grid-cols-3 md:grid-cols-9 grid-flow-row">
       <img class="col-span-3" src="@/assets/IMG_20180308_181618.jpg" alt="Sam and Karen">
-      <iframe class="col-span-3 w-full h-full"
-        referrerpolicy="no-referrer-when-downgrade" 
+      <iframe class="col-span-3 w-full h-full" referrerpolicy="no-referrer-when-downgrade"
         src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBkgrMtxoRns1F2oyRmcfvQJIgDqB2uvPw&center=53.744014145893225, -0.3390485200832282&zoom=19&maptype=satellite">
       </iframe>
-      <div class="col-span-3">
-        <p>We met for the first in person in March 2018 for our very first date. The magic first began in an open and safe spot in Queen Victoria Square.</p>
+      <div class="col-span-3 text-xs">
+        <p>
+          "What a crazy day this was. I suggested we start small and simple with a hot drink in the city centre. In the
+          end we walked and talked all across town! The spark between us was pretty clear and the days following it were
+          like nothing I had ever experienced." - Sam
+        </p>
+        <br />
+        <p>
+          "We decided to meet up at Cafe Nero in Victoria's Square, a public safe place to meet up. The very first statement he blurted out was not hello or good day, but "OH MY GOD! You're so short!!". Thinking about it makes me chuckle even now. " - Karen
+        </p>
       </div>
     </div>
-    <div class="bg-white-dark h-80">First holiday in the Philippines</div>
-    <div class="bg-white h-80">Proposal</div>
-    <div class="bg-white-dark h-80">Sofia Joinning The Family</div>
-    <div class="bg-white h-80">Our Next Experience Together</div>
+    <div class="bg-white-dark text-4xl col-start-2 col-span-4">First holiday in the Philippines - January 2019</div>
+    <div class="bg-white-dark grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <iframe class="col-span-3 w-full h-full" referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBkgrMtxoRns1F2oyRmcfvQJIgDqB2uvPw&maptype=satellite&q=cebu+island">
+      </iframe>
+      <div class="col-span-3 text-xs">
+        <p>
+          "It's hard to believe how fast our relationship was going. In under a year of meeting we had already flown to
+          the beautiful Philippines together. I had already loved pinoy cuisine before but this holiday was just heaven
+          for me for food. Huge culture shocks for sure but I gained a much deeper appreciation for the culture." - Sam
+        </p>
+        <br />
+        <p>
+          "There were a lot of first times for Sam during the holiday from the first stomach bug to riding his first zipline. Most importantly, first time meeting my parents and family in Cebu City. All of them boys even went on a little fishing trip. My family loved him so much because they can see how happy I am around Sam. We went to Camotes Island for a whole family trip touring beaches and caves. Then, both of us went on a trip to Malapascua Island and Kalanggaman Island. Honestly, Sam enjoyed the trips to different islands, but I think he enjoyed mostly the food trip especially Sugbo Mercado." - Karen
+        </p>
+      </div>
+      <img class="col-span-3" src="@/assets/event2_1.jpg" alt="Sam and Karen">
+    </div>
+    <div class="bg-white-dark grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <img class="col-span-3" src="@/assets/event2_2.jpg" alt="Sam and Karen">
+      <img class="col-span-3" src="@/assets/event2_3.jpg" alt="Sam and Karen">
+      <img class="col-span-3" src="@/assets/event2_4.jpg" alt="Sam and Karen">
+    </div>
+    <div class="bg-white text-4xl col-start-2 col-span-4">Proposal - January 2020</div>
+    <div class="bg-white grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <div class="col-span-3 text-xs">
+        <p>
+          "It was after our second holiday in the Philippines in November 2019 that I started thinking about our life
+          together. I had even looked at rings just before the holiday and finally bought one in January. The right
+          moment to propose presented itself pretty quickly! Karen was already so happy that night and had been having a
+          great time with her friends so I made a speech during the night and read it out when I finally had mustered up
+          the courage to propose." - Sam
+        </p>
+        <br />
+        <p>
+          "It was the night of my leaving do. Due to the pandemic, my Filipino ICU teammates didn't get the chance to have a Christmas gathering so we ended up doing it during my leaving do. All of us had a wonderful dinner at The Minerva then went to continue the celebration at Ate Lynette and Kuya Arlie's house. Everyone was having a good time with food, drinks, and the best entertainment Karaoke! No one had any clue as to what will happen next. Then, Sam said to me he's going to make a speech so the karaoke mic was passed to him. Everyone thought it was only a cheesy thank you speech until he said "I stopped myself asking questions instead I have only one question to ask, Will you marry me?". Everyone was screaming at the top of their lungs! Of course, I said yes!" - Karen
+        </p>
+      </div>
+      <img class="col-span-3" src="@/assets/event3_2.jpg" alt="Sam and Karen">
+      <iframe class="col-span-3 w-full h-full" src="https://www.youtube-nocookie.com/embed/1OyZzPKP8yc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="bg-white-dark text-4xl col-start-2 col-span-4">Sofia Joinning The Family - July 2021</div>
+    <div class="bg-white-dark grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <img class="col-span-3" src="@/assets/event4_1.jpg" alt="Sam and Karen">
+      <div class="col-span-3 text-xs">
+        <p>
+          "This is one of the toughest, most exciting moments in my life I think I'll ever have. For better or worse we had Sofia during the heights of covid which meant no friends or family could help us while Karen was in hospital. I only got to visit 1 hour each day for the 10 days Karen was there. Both Karen and Sofia fought hard to get well enough to leave the hospital which made an unforgettable 2 passanger drive home." - Sam
+        </p>
+        <br />
+        <p>
+          "Our little bundle of joy whose gender we haven't revealed to our friends until she was born. At work, a lot of people suggested doing a betting game lol. Oh my, the struggles of being first-time parents are not to be taken lightly. It was a difficult birthing experience to begin with from delayed discharge due to complications to NICU admissions. In the end, we have overcome such challenged. Sofia is beautiful in every aspect." - Karen
+        </p>
+      </div>
+      <iframe class="col-span-3 w-full h-full" src="https://www.youtube-nocookie.com/embed/dV294LIUnos" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="bg-white-dark grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <img class="col-span-3" src="@/assets/event4_2.jpg" alt="Sam and Karen">
+      <iframe class="col-span-3 w-full h-full" referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBkgrMtxoRns1F2oyRmcfvQJIgDqB2uvPw&center=53.743871748194984, -0.3554770775859294&zoom=19&maptype=satellite">
+      </iframe>
+      <img class="col-span-3" src="@/assets/event4_3.jpg" alt="Sam and Karen">
+    </div>
+    <div class="bg-white text-4xl">Our Next Experience Together - March 2023</div>
+    <div class="bg-white grid grid-cols-3 md:grid-cols-9 grid-flow-row">
+      <div class="col-span-3 text-xs">
+        <p>
+          I still need to do this bit lol
+        </p>
+      </div>
+      
+    </div>
     <div class="bg-white-dark">
       <h2 class="text-5xl text-primary-darkest mb-4">Please enter your details below</h2>
       <form>
