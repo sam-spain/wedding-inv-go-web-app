e89d0ecb9f79f1fff66c5015ee3908344aa39335
Sam Spain <sam@samspain.com>
2023-05-06 14:47:13 +0100

    Remove interaction with server

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <RouterView />
   <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-black sm:text-center">2022 <a href="https://samspa.in" class="hover:underline">Made by Sam</a>.
+    <span class="text-sm text-black sm:text-center">2023 <a href="https://samspa.in" class="hover:underline">Made by Sam</a>.
     </span>
     <nav class="flex flex-wrap items-center mt-3 text-sm text-black sm:mt-0">
         <RouterLink class="mr-4 hover:underline md:mr-6 " to="/">Home</RouterLink>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This website is an archive of our wedding invitation. All guest data has been deleted and therefore no guest names can be used.</h1>
   </div>
 </template>
 
